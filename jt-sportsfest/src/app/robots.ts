93db908f6ef3aedc8039654e2c1ec3dcb6368321
ahmad-56 <ahmad392.hassan@gmail.com>
2026-08-28 10:57:29 +0500

    Update robots.ts

--- jt-sportsfest/src/app/robots.ts
+++ jt-sportsfest/src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = "https://yourdomain.com";
+  const siteUrl = "https://lgsjtsportsfest.com";
 
   return {
     rules: {
